1ea6085661f3c2b2e6d57c7a52a054f78a71c1c7
unknown <yhlee89.dho@gmail.com>
2020-01-16 16:05:55 -0600

    initiation + server.js

---
+++ Develop/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
